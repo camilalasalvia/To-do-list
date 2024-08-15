5b99944658e47c92d961884c1d82e9fcfc5c611b
Camila <c.lasalvia12@gmail.com>
2024-08-14 21:58:58 -0300

    folders rearranged again

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { ToDoApp } from './ToDoApp';
+import { ToDoApp } from './components/ToDoApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
